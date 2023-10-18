f595da631f295a9ee63545c0b510a14978161293
Sai Swarup Patnaik <108411440+saiswarup2001@users.noreply.github.com>
2023-10-18 16:14:48 +0000

    dataTypes in js

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+ 
+// alert(3+3) WE are using node js, not browser
+
+console.log( 3
+    +3
+     ); // code readability should be high
+
+console.log("Sai");
+
+let name = "Sai Swarup"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/ false
+// null => standalone value ==> it is an object
+// undefined => 
+// symbol => to find unique 
+
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
