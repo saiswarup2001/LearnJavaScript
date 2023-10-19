e9e08c5ac942f05ec22d4e992070a487270734d4
Sai Swarup Patnaik <108411440+saiswarup2001@users.noreply.github.com>
2023-10-19 15:50:48 +0000

    conversions

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -33,3 +33,51 @@ let stringNumber = String(number)
 console.log("--------------------String Conversion------------------");
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+/* ============================== Operations =============================================== */
+
+let value  =  3
+let negValue  = -value
+console.log("================================== operations =============================");
+console.log(negValue);
+
+/*
+console.log(2+2); //additions
+console.log(2-2); //substract
+console.log(2*2); //multiply
+console.log(2**3); //power
+console.log(2/2); //divide
+console.log(2%3); //modulus
+*/
+
+let str1 = 'Hello'
+let str2 = " Sai"
+ 
+let str3 = str1 + str2
+console.log("String: "+ str3);
+
+/* ============================= Type Conversion =================================*/
+console.log("============ typo ======================");
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "2");//32
+
+console.log("=============tricky ==========");
+console.log(true); //true
+console.log(+true); //1
+console.log(+""); //0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log("game counter ++: "+ gameCounter);
+++gameCounter
+console.log("++ game counter: "+ gameCounter);
+
+// link to study about postfix or prfix: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
