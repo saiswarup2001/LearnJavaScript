f9d8744fa801a6c789cb4ec146a530e728f4ec21
Sai Swarup Patnaik <saiswarup2001@gmail.com>
2023-10-23 19:22:53 +0530

    obj destructuring

---
+++ 02_Basics/05_objectsInDeep.js
@@ -0,0 +1,43 @@
+//destructuring of object
+
+const course = {
+    coursename: "Js in Hindi",
+    price: "999",
+    courseInstructor: "Sai"
+}
+
+// course.courseInstructor
+//clean code to print
+
+const {CI} = course
+console.log(CI);
+
+//API Concepts
+//JSON 
+// {
+//     "name": "sai",
+//     "course": "js",
+//     "price": "free"
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//react
+const navbar = ({company}) =>{
+
+}
+
+navbar(company = "hitesh")
+*/
